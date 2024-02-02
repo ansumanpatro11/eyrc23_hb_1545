974bdc18b6a6ec96a6376fffaf401de57b3710ec
ansuman <ansumanpatro.rohan@gmail.com>
2024-02-02 20:01:50 +0530

    feedback fast done

--- hb_controller/feedback_for_fast.py
+++ hb_controller/feedback_for_fast.py
@@ -62,6 +62,8 @@ class FeedBack(Node):
         self.topright=None
         self.bottomleft=None
         self.bottomright=None
+        self.caliberated_image=None
+        self.arena_corners=[]
         # bot_ids=[1,2,3]
         # self.bot_paths=[[] for _ in range(len[bot_ids])]
         self.distorted_image = None
@@ -69,19 +71,24 @@ class FeedBack(Node):
         
         self.corners=[]
         self.ids=[]
+        self.previous_tL=None
+        self.previous_tR=None
+        self.previous_bR=None
+        self.previous_bL=None
+        
         
     def image_callback(self,msg):
         # Convert the ROS image message to an OpenCV image.
         self.distorted_image = self.bridge.imgmsg_to_cv2(msg, "bgr8")    
         # Publish the detected ArUco marker's pose.
-        if self.distorted_image is not None:
-            self.caliberated_image=camera_caliberation(self.distorted_image)
+        # if self.distorted_image is not None:
+        # self.caliberated_image=camera_caliberation(self.distorted_image)
 
-            self.corners,self.ids=aruco_detect(thresholding(self.caliberated_image))
-            self.topleft,self.topright,self.bottomleft,self.bottomright=get_corners(self.corners,self.ids)
-            if self.topleft is not None and self.topright is not None and self.bottomleft is not None and self.bottomright is not None:
-                self.arena_corners=np.float32([self.topleft,self.topright,self.bottomleft,self.bottomright])
-                self.transformed_image=perspective_transform(self.caliberated_image,self.arena_corners)
+        # self.corners,self.ids=aruco_detect((self.caliberated_image))
+        # self.topleft,self.topright,self.bottomleft,self.bottomright=get_corners(self.corners,self.ids)
+        # if self.topleft is not None and self.topright is not None and self.bottomleft is not None and self.bottomright is not None:
+        #     self.arena_corners=np.float32([self.topleft,self.topright,self.bottomleft,self.bottomright])
+        #     self.transformed_image=perspective_transform(self.caliberated_image,self.arena_corners)
             # return self.transformed_image
 
     # def image_converter(self,image):
@@ -109,14 +116,39 @@ def main(args=None):
                 # print(f"corners={corners}")
                 
                 bot_ids=[11,14,2]
-                
-                # Check if marker ID 1 is detected.
+                fb.caliberated_image=camera_caliberation(fb.distorted_image)
+
+                fb.corners,fb.ids=aruco_detect(fb.caliberated_image)
+                fb.topleft,fb.topright,fb.bottomleft,fb.bottomright=get_corners(fb.corners,fb.ids)
+                if fb.topleft is not None and fb.topright is not None and fb.bottomleft is not None and fb.bottomright is not None:
+                    fb.arena_corners=np.float32([fb.topleft,fb.topright,fb.bottomleft,fb.bottomright])
+                    fb.previous_bL=fb.bottomleft
+                    fb.previous_bR=fb.bottomright
+                    fb.previous_tL=fb.topleft
+                    fb.previous_tR=fb.topright
+                    
+                    
+                if fb.bottomleft is None:
+                    fb.bottomleft=fb.previous_bL                   
+                if fb.topright is None:
+                    fb.topright=fb.previous_tR
+                if fb.bottomright is None:
+                    fb.bottomright=fb.previous_bR
+                if fb.topleft is None:
+                    fb.topleft=fb.previous_tL
+                fb.transformed_image=perspective_transform(fb.caliberated_image,fb.arena_corners)
+
+
             
                 
-                for i in range(len(fb.corners)):
+                # Check if marker ID 1 is detected.
+                corners,ids=aruco_detect(thresholding(fb.transformed_image))
+                
+                for i in range(len(corners)):
                     for j in range(len(bot_ids)):
-                        if fb.ids[i][0]==bot_ids[j]:
-                            arr=fb.corners[i][0]
+                        if ids[i][0]==bot_ids[j]:
+                            # print(ids,corners)
+                            arr=corners[i][0]
                             # Calculate the center coordinates and orientation (theta) of the detected marker
                             centre_x_opencv = sum(arr[:, 0]) / 4
                             centre_y_opencv = sum(arr[:, 1]) / 4
@@ -141,6 +173,8 @@ def main(args=None):
                             fb.msg[j].y=centre_y
                             fb.msg[j].theta=theta
                             fb.pub1.publish(fb.msg[j])
+                            cv2.imshow('frame',fb.transformed_image)
+                            cv2.waitKey(1)
                             
 
                 
